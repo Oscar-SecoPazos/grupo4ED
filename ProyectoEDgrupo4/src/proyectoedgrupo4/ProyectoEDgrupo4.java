43c27a6ab023a9e0dffaa020d51d515641a75290
AdrianGomezVillar <73839757+AdrianGomezVillar@users.noreply.github.com>
2020-11-30 16:30:21 +0100

    prueba2

--- ProyectoEDgrupo4/src/proyectoedgrupo4/ProyectoEDgrupo4.java
+++ ProyectoEDgrupo4/src/proyectoedgrupo4/ProyectoEDgrupo4.java
@@ -5,6 +5,8 @@
  */
 package proyectoedgrupo4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author DAM127
@@ -16,6 +18,49 @@ public class ProyectoEDgrupo4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner teclado=new Scanner(System.in);
+        int opc=0,huevos=0;
+        double chorizo=0;
+        do{
+            do{
+                System.out.println("1.- .\n" +
+                                   "2.- .\n" +
+                                   "3.- .\n" +
+                                   "4.- .\n" +
+                                   "5.- .\n" +
+                                   "6.- .\n" +
+                                   "7.- .\n"+
+                                   "8.-Salir.");
+                opc = teclado.nextInt();
+            }while((opc>7)||(opc<1));
+            switch(opc){
+                case 1:
+                    
+                break;
+                case 2:
+                    
+                break;
+                case 3:
+                    
+                break;
+                case 4:
+                    
+                break;
+                case 5:
+                    
+                break;
+                case 6:
+                    
+                break;
+                
+                case 7:
+                    System.out.println("Saliendo del programa.");
+                break;
+                default:
+                    System.out.println("Error");
+                break;
+            }
+        }while(opc!=7);
     }
     
 }
